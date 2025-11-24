9d912849cf1cae30c06358f7d5392b0076991512
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-11-24 14:38:05 +0000

    Added product sorting by price , name ,category & rating.

--- products/views.py
+++ products/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render, redirect,reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product,Category
+from django.db.models.functions import Lower
 
 # Create your views here.
 
 def product_list(request):
     """ View to display list of products , enabling both search and filter functionality."""
-    
+
     products = Product.objects.all()
     total_products = products.count()
     query = None
     category_names = []
     category_objects = Category.objects.none()
+    sort = None
+    direction = None
+
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower("name"))
+
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+        products = products.order_by(sortkey)
     
     if 'category' in request.GET:
         category_names = request.GET['category'].split(',')
@@ -29,6 +45,8 @@ def product_list(request):
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         products = products.filter(queries)
         total_products = products.count()
+    
+    current_sorting = f'{sort}_{direction}' 
 
     context = {
         'products': products,
@@ -36,6 +54,7 @@ def product_list(request):
         'search_term': query,
         'current_categories': category_names,
         'category_objects': category_objects,
+        'current_sorting':current_sorting
     }
     return render(request, 'products/products.html', context)
 
