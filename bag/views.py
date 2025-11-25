71b25a87db2cea9e8f92d3c9a0dedd56f316acf7
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-11-25 15:54:21 +0000

    Updated qty selector by wrapping it in a form. Added bag functionality.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ def shopping_bag(request):
     """ View rendering shopping bag page """
 
     return render(request, 'bag/shopping_bag.html')
+
+
+def add_items(request, item_id):
+    """ View quantity of indiviual items added to bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
