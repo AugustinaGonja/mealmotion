5517a4bb6cb5b3176c4806cb998304a5e93e1acf
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-11-26 16:13:21 +0000

    Amended bag layout & context.

--- bag/context.py
+++ bag/context.py
@@ -13,11 +13,14 @@ def bag_contents(request):
     for item_id , quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
+        item_total = quantity * product.price
         product_count += quantity
+        
         bag_items.append({
             'item_id':item_id,
             'quantity':quantity,
             'product':product,
+            'item_total':item_total
             })
         
     if total < settings.FREE_DELIVERY_THRESHOLD:
