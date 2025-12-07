6330fd925f288ed76b6cddf34a5bd931d221174e
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-07 18:25:56 +0000

    Added checkout success and views to allow orders to be saved.

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render,redirect,reverse
+from django.shortcuts import render,redirect,reverse,get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+
 from .forms import OrderForm
+from products.models import Product
+from .models import Order, OrderLineItem
+
 from bag.context import bag_contents
+
 import stripe
 
 # Create your views here.
@@ -11,28 +16,82 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag= request.session.get('bag', {})
-    if not bag :
-        messages.error (request , "There are currently no items in you bag")
-        return redirect(reverse('products'))
-    
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)  
-    stripe.api_key = stripe_secret_key
+    if request.method == 'POST':
+        bag= request.session.get('bag', {})
 
-    # Payment Intents 
-    intent = stripe.PaymentIntent.create(
-        amount = stripe_total,
-        currency = settings.STRIPE_CURRENCY
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'contact_number': request.POST['contact_number'],
+            'town_or_city': request.POST['town_or_city'],
+            'address_line_1': request.POST['address_line_1'],
+            'address_line_2': request.POST['address_line_2'],
+            'post_code': request.POST['post_code'],
+            'county': request.POST['county'],
+            'country':request.POST['country'],
+        }
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
+        order_form = OrderForm(form_data)
+
+        if order_form.is_valid():
+            order = order_form.save()
+
+            bag = request.session.get('bag', {})
+
+            for item_id, quantity in bag_contents():
+                product = Product.objects.get(id=item_id)
+
+                OrderLineItem.objects.create(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                )
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'Theres an error in your form. Check your information and try again.')
+    else:
+        bag= request.session.get('bag', {})
+        if not bag :
+            messages.error (request , "There are currently no items in you bag")
+            return redirect(reverse('products'))
+        
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)  
+        stripe.api_key = stripe_secret_key
+
+        # Payment Intents 
+        intent = stripe.PaymentIntent.create(
+            amount = stripe_total,
+            currency = settings.STRIPE_CURRENCY
+        )
+
+        order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?') 
+            
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form' : order_form,
+            'stripe_public_key':stripe_public_key,
+            'client_secret':intent.client_secret,
+        }
+
+        return render(request, template, context)
+
+def checkout_success(request, order_number):
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order Successful ! Your order number is {order_number}.Confirmation will be sent to {order.email}')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
     context = {
-        'order_form' : order_form,
-        'stripe_public_key':stripe_public_key,
-        'client_secret':intent.client_secret,
+        'order':order
     }
 
     return render(request, template, context)
