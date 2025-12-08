db5f50b859f059c04a6d2b3463a523a7e1403b0e
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-08 18:15:39 +0000

    Set up webhooks and tested them.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,6 +9,20 @@ class StripeWH_Handler:
     def handle_event(self, event):
        """Handle unexpected webhook event"""
        return HttpResponse (
+          content= f'Unhandled webhook recieved: {event["type"]}',
+          status=200
+       )
+    
+    def handle_payment_intent_succeded(self, event):
+       """Handle payment_intent_succeded webhook"""
+       return HttpResponse (
+          content= f'Webhook recieved: {event["type"]}',
+          status=200
+       )
+    
+    def handle_payment_intent_failed(self, event):
+       """Handle payment_intent_failed webhook"""
+       return HttpResponse (
           content= f'Webhook recieved: {event["type"]}',
           status=200
        )
