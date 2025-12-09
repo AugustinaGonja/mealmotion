a1ce3f2ca1e97c10c3f2dfeae73f2214c0cfa320
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-09 12:27:59 +0000

    Added profile app - models , views,urls and templates.

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField # type: ignore
 
@@ -10,6 +11,7 @@ from django_countries.fields import CountryField # type: ignore
 
 class Order(models.Model):
     order_number = models.CharField(max_length=50, unique=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True , blank = True, related_name='orders')
     full_name = models.CharField(max_length=255, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False )
     contact_number = models.CharField(max_length=20, null=False, blank=False)
