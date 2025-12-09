0f89fc2ce2f558bd0252962b04eab51a9dbd0603
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-09 22:15:17 +0000

    Added profile form ,templates and views updated.

---
+++ profiles/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import UserProfile
+
+# Django Form Customisation
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+        
+    def __init__(self, *args , **kwargs ):
+        """Remove and replace labels with Placeholders """
+        super().__init__(*args , **kwargs)
+        placeholders = {
+            'default_contact_number': 'Contact Number',
+            'default_town_or_city': 'Town or City',
+            'default_address_line_1': 'Address Line 1',
+            'default_address_line_2': 'Address Line 2',
+            'default_post_code': 'Post Code',
+            'default_county': 'County',
+            'default_country':'Country',
+        }
+        
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+            
--- profiles/models.py
+++ profiles/models.py
@@ -12,12 +12,12 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     default_contact_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label = 'Select a Country', max_length=40, null =True , blank =True)
+    default_post_code = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_address_line_1 = models.CharField(max_length=80, null=True, blank=True)
     default_address_line_2 = models.CharField(max_length=255, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_post_code = models.CharField(max_length=20, null=True, blank=True)
     default_county = models.CharField(max_length=40, null =True , blank =True)
-    default_country = CountryField(blank_label = 'Select a Country', max_length=40, null =True , blank =True)
 
     def __str__(self):
         return self.user.username
