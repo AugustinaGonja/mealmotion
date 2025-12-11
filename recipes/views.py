25ba3a56b54b03febf6ccddf7e935807c2ea8d6a
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-11 00:43:43 +0000

    Added content to recipe template.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
+def recipe_list(request):
+    """ View to display list of recipes , enabling both search and filter functionality."""
+    recipes = Recipe.objects.all()
+    
+    context = {
+        'recipes' : recipes
+    }
+
+    return render(request, 'recipes/recipes.html', context)
