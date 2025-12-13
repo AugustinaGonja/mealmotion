c27ef803c0069b8cc1206b9d19ac277ee51d6288
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-13 16:08:35 +0000

    Fix static files storage for Heroku - attempt.

--- mealmotion/settings.py
+++ mealmotion/settings.py
@@ -176,9 +176,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static Files 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media
 MEDIA_URL = '/media/'
